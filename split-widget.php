97947fc6553e17d0139033729ace5722658fe23a
Kazi Sadib Reza <kazisadibreza@gmail.com>
2025-01-14 21:11:39 +0600

    first commit

---
+++ split-widget.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Split Widget
+Plugin URI:
+Description: Elementor Widgets and Theme Builder Elements
+Version: 1.0.15
+Author: UiCore
+Author URI: https://uicore.co
+License: GPL3
+Text Domain: uicore-elements
+Domain Path: /languages
+ * Elementor requires at least: 3.19.2
+ * Elementor tested up to: 3.26.3
+*/
